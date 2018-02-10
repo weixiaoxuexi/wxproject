d836bd941a0de2d6e8c06fc0406bb5762a82350a
weixiaoxuexi <xunvvv@126.com>
2018-02-10 12:12:56 +0800

    project init

---
+++ src/main/java/com/yueqinxuexi/wx/wxproject/WxprojectApplication.java
@@ -0,0 +1,12 @@
+package com.yueqinxuexi.wx.wxproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxprojectApplication.class, args);
+	}
+}
