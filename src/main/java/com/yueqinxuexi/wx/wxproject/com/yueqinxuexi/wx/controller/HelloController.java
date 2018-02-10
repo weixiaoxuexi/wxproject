9446f5e57624951e33d882e2e0430ef1e1c0a8f8
weixiaoxuexi <xunvvv@126.com>
2018-02-10 23:53:34 +0800

    完成helloworld

--- src/main/java/com/yueqinxuexi/wx/wxproject/com/yueqinxuexi/wx/controller/HelloController.java
+++ src/main/java/com/yueqinxuexi/wx/wxproject/com/yueqinxuexi/wx/controller/HelloController.java
@@ -1,4 +1,14 @@
 package com.yueqinxuexi.wx.wxproject.com.yueqinxuexi.wx.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(){
+        return "Hello World!";
+    }
 }
